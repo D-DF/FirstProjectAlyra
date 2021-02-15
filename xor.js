d63bb52b5143423b31f3b034210cb355eef56b80
David De Freitas <59915978+D-DF@users.noreply.github.com>
2021-02-15 16:58:12 +0100

    Delete xor.js

--- xor.js
+++
@@ -1,8 +0,0 @@
-let xor1 = true;
-let xor2 = true;
-
-if (!xor1 && xor2) {
-  console.log("Oui c'est vrai")
-} else {
-  console.log("Deux conditions qui sont vrai, nous donnes false comme résultat, voilà le XOR.")
-}
